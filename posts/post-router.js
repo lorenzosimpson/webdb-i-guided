7a884869215c072b592bfea1710481c0a5e477dd
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-11-04 13:58:05 -0500

    adds query to get posts

--- posts/post-router.js
+++ posts/post-router.js
@@ -1,12 +1,18 @@
 const express = require('express');
 
 // database access using knex
-const db = require('../data/db-config.js');
+const knex = require('../data/db-config.js');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-
+    // list of posts
+    // SELECT * from Posts;
+    knex.select('*').from('posts')
+    .then(posts => {
+        res.status(200).json(posts)
+    })
+    .catch(err => res.status(500).json({ error: 'Failed to get posts from database'}))
 });
 
 router.get('/:id', (req, res) => {
